78fd29cd5456d5270c578b10c77b299f2cc1353b
Kirill <kiril.vlasov.96@gmail.com>
2021-06-15 11:36:55 +0300

    Changing menuCard generation using Rest operator

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -149,14 +149,15 @@ window.addEventListener('scroll', showModalByScroll);
 const menuItem = document.querySelector('.menu__item');
 
 class MenuCard{
-    constructor(src, alt, title, descr, price, parentSelector){
+    constructor(src, alt, title, descr, price, parentSelector, ...classes){
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector);
-        this.rate = 2.6;
+        this.rate = 2.5;
         this.changeToUAH();
     }
 
@@ -166,8 +167,12 @@ class MenuCard{
 
     render(){
         const element = document.createElement('div');
+        if(this.classes.length === 0){
+            this.element = 'menu__item';
+            element.classList.add(this.element);
+        }
+        this.classes.forEach(className => {element.classList.add(className);});
         element.innerHTML = `
-        <div class="menu__item">
             <img src=${this.src} alt=${this.alt}>
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -175,8 +180,7 @@ class MenuCard{
             <div class="menu__item-price">
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-        </div>
-    </div>`;
+            </div>`;
         this.parent.append(element);
     }
 }
@@ -186,7 +190,7 @@ new MenuCard(
     "vegy",
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    30,    
+    14,    
     '.menu .menu__field .container'
 ).render();
 
@@ -195,7 +199,7 @@ new MenuCard(
     "elite",
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    50,    
+    18,    
     '.menu .menu__field .container'
 ).render();
 
@@ -204,6 +208,7 @@ new MenuCard(
     "post",
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    40,    
+    12,    
     '.menu .menu__field .container'
 ).render();
+
