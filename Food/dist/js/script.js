54811607dc0a87aaf21356c3987d271c0bc82723
Kirill <kiril.vlasov.96@gmail.com>
2021-06-23 17:35:32 +0300

    Connecting JSON Server

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -143,71 +143,81 @@ window.addEventListener('DOMContentLoaded', () => {
     // Classes for menu cards
     const menuItem = document.querySelector('.menu__item');
 
-    class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.descr = descr;
-            this.price = price;
-            this.classes = classes;
-            this.parent = document.querySelector(parentSelector);
-            this.rate = 2.5;
-            this.changeToBYN();
-        }
+    // class MenuCard {
+    //     constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+    //         this.src = src;
+    //         this.alt = alt;
+    //         this.title = title;
+    //         this.descr = descr;
+    //         this.price = price;
+    //         this.classes = classes;
+    //         this.parent = document.querySelector(parentSelector);
+    //         this.rate = 2.5;
+    //         this.changeToBYN();
+    //     }
 
-        changeToBYN() {
-            this.price = this.price * this.rate;
-        }
+    //     changeToBYN() {
+    //         this.price = this.price * this.rate;
+    //     }
 
-        render() {
-            const element = document.createElement('div');
-            if (this.classes.length === 0) {
-                this.element = 'menu__item';
-                element.classList.add(this.element);
+    //     render() {
+    //         const element = document.createElement('div');
+    //         if (this.classes.length === 0) {
+    //             this.element = 'menu__item';
+    //             element.classList.add(this.element);
+    //         }
+    //         this.classes.forEach(className => {
+    //             element.classList.add(className);
+    //         });
+    //         element.innerHTML = `
+    //         <img src=${this.src} alt=${this.alt}>
+    //         <h3 class="menu__item-subtitle">${this.title}</h3>
+    //         <div class="menu__item-descr">${this.descr}</div>
+    //         <div class="menu__item-divider"></div>
+    //         <div class="menu__item-price">
+    //             <div class="menu__item-cost">Цена:</div>
+    //             <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+    //         </div>`;
+    //         this.parent.append(element);
+    //     }
+    // }
+
+    const getResource = async (url) => {
+        const res = await fetch(url);
+          if (!res.ok){
+            throw new Error(`Could not fetch ${url}, status ${res.status}`);
             }
-            this.classes.forEach(className => {
-                element.classList.add(className);
-            });
+    return await res.json();
+    };
+
+    // getResource('http://localhost:3000/menu')
+    // .then(data => {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //     });
+    // });
+
+    getResource('http://localhost:3000/menu')
+        .then(data => createCard(data));
+
+    const createCard = function(data) {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            const element = document.createElement('div');
+
+            element.classList.add('menu__item');
             element.innerHTML = `
-            <img src=${this.src} alt=${this.alt}>
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">${this.descr}</div>
+            <img src=${img} alt=${altimg}>
+            <h3 class="menu__item-subtitle">${title}</h3>
+            <div class="menu__item-descr">${descr}</div>
             <div class="menu__item-divider"></div>
             <div class="menu__item-price">
                 <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-            </div>`;
-            this.parent.append(element);
-        }
-    }
-
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        14,
-        '.menu .menu__field .container'
-    ).render();
-
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        18,
-        '.menu .menu__field .container'
-    ).render();
-
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        12,
-        '.menu .menu__field .container'
-    ).render();
+                <div class="menu__item-total"><span>${price*2.5}</span> руб/день</div>
+            </div>
+            `;
+            document.querySelector('.menu .container').append(element);
+        });
+    };
 
     const forms = document.querySelectorAll('form');
 
@@ -218,10 +228,21 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {                 
+            method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },  
+        body: data
+    });
+    return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
 
             e.preventDefault();
@@ -235,25 +256,11 @@ window.addEventListener('DOMContentLoaded', () => {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
-            // const request = new XMLHttpRequest();
-            // request.open('POST', 'http://localhost:5000', true);
-
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries));
 
-            fetch('index.js',{
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },  
-                body: JSON.stringify(object)
-                
-            })
-            .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data.response);
                 showThanksModal(message.success);
@@ -292,5 +299,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-});
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 
+});
